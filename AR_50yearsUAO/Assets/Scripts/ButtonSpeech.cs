5bef100c9ce96fb6b0d1d40ef100e9036f0beb41
alejandrodomingueztorres <85324176+alejandrodomingueztorres@users.noreply.github.com>
2025-10-05 17:55:13 -0500

    Configuración del asistente
    Se crea toda la configuración del asistente y se agrega el modelo del mismo

---
+++ AR_50yearsUAO/Assets/Scripts/ButtonSpeech.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ButtonSpeech : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public UnityEvent OnButtonPressed;
+    public UnityEvent OnButtonRealeased;
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnButtonPressed?.Invoke();
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        OnButtonRealeased?.Invoke();
+    }
+}
