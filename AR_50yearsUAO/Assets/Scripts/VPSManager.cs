3d3d1db51c35dfa792a6a508aa4b08e9659240b9
jose2231411 <jose.criollo@uao.edu.co>
2025-10-15 14:09:44 -0500

    Reapply "Geolocalización VPS"
    This reverts commit 58f9019288280f309eb63d0d39d312892eaae2a1.

--- AR_50yearsUAO/Assets/Scripts/VPSManager.cs
+++ AR_50yearsUAO/Assets/Scripts/VPSManager.cs
@@ -3,25 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
-//using Google.XR.ARCoreExtensions;
+using Google.XR.ARCoreExtensions;
 using System;
 
 public class VPSManager : MonoBehaviour
 {
-    //private AREarthManager earthManager = new AREarthManager();
+    [SerializeField] private AREarthManager earthManager;
 
-    [SerializeField]
-    public struct GeospatialObject
+    [System.Serializable] public struct GeospatialObject
     {
         public GameObject ObjectPrefab;
         public EarthPosition EarthPosition;
     }
-    [SerializeField]
-    public struct EarthPosition
+    [System.Serializable] public struct EarthPosition
     {
-        public double latitude;
-        public double longitude;
-        public double altitude;
+        public double Latitude;
+        public double Longitude;
+        public double Altitude;
     }
 
     [SerializeField] private ARAnchorManager aRAnchorManager;
@@ -34,45 +32,45 @@ public class VPSManager : MonoBehaviour
 
     private void VerifyGeospatialSupport()
     {
-        //var result = earthManager.IsGeospatialModeSupported(GeospatialMode.Enabled);
-        //switch (result)
-        //{
-        //    case FeatureSupported.Supported:
-        //        Debug.Log("Ready to use VPS");
-        //        PlaceObjects();
-        //        break;
-        //    case FeatureSupported.Unknown:
-        //        Debug.Log("Unknown...");
-        //        Invoke("VerifyGeospatialSupport", 5.0f);
-        //        break;
-        //    case FeatureSupported.Unsupported:
-        //        Debug.Log("VPS Unsupported");
-        //        break;
-        //}
+        var result = earthManager.IsGeospatialModeSupported(GeospatialMode.Enabled);
+        switch (result)
+        {
+            case FeatureSupported.Supported:
+                Debug.Log("Ready to use VPS");
+                PlaceObjects();
+                break;
+            case FeatureSupported.Unknown:
+                Debug.Log("Unknown...");
+                Invoke("VerifyGeospatialSupport", 5.0f);
+                break;
+            case FeatureSupported.Unsupported:
+                Debug.Log("VPS Unsupported");
+                break;
+        }
     }
 
     private void PlaceObjects()
     {
-        //if (earthManager.EarthTrackingState != TrackingState.Tracking)
-        //{
-        //    var geospatialPose = earthManager.CameraGeospatialPose;
+        if (earthManager.EarthTrackingState != TrackingState.Tracking)
+        {
+            var geospatialPose = earthManager.CameraGeospatialPose;
 
-        //    foreach (var obj in geospatialObjects)
-        //    {
-        //        var earthPosition = obj.EarthPosition;
-        //        var objAnchor = ARAnchorManagerExtensions.AddAnchor(
-        //            aRAnchorManager,
-        //            earthPosition.latitude,
-        //            earthPosition.longitude,
-        //            earthPosition.altitude,
-        //            Quaternion.identity);
-        //        Instantiate(obj.ObjectPrefab, objAnchor.transform);
-        //    }
-        //}
+            foreach (var obj in geospatialObjects)
+            {
+                var earthPosition = obj.EarthPosition;
+                var objAnchor = ARAnchorManagerExtensions.AddAnchor(
+                    aRAnchorManager,
+                    earthPosition.Latitude,
+                    earthPosition.Longitude,
+                    earthPosition.Altitude,
+                    Quaternion.identity);
+                Instantiate(obj.ObjectPrefab, objAnchor.transform);
+            }
+        }
 
-        //else if (earthManager.EarthTrackingState == TrackingState.None)
-        //{
-        //    Invoke("PlaceObjects", 5.0f);
-        //}
+        else if (earthManager.EarthTrackingState == TrackingState.None)
+        {
+            Invoke("PlaceObjects", 5.0f);
+        }
     }
 }
